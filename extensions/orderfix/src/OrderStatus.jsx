05c73624f8301dc5c04c387b6d45aa88e4703313
MarceloDevArg <sponsorfull@gmail.com>
2026-06-03 19:05:18 -0300

    Revert "Move address editing into customer account extension"
    This reverts commit 9e608f6f2a3b47a66784b2d14f22c39a0fadc6db.

--- extensions/orderfix/src/OrderStatus.jsx
+++
@@ -1,7 +0,0 @@
-import '@shopify/ui-extensions/preact';
-import {render} from 'preact';
-import {OrderFix} from './OrderFix.js';
-
-export default async () => {
-  render(<OrderFix />, document.body);
-};
