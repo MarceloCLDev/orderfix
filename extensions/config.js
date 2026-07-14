ebe5cc5c242b65a3e772d165a3d835d0cc89d6dc
MarceloCLDev <marcelo.n@perfumeventures.com>
2026-07-14 20:17:24 -0300

    Updating to prod app url

--- extensions/config.js
+++ extensions/config.js
@@ -1,2 +1,1 @@
-//export const APP_URL = 'https://orderfix-production-22d8.up.railway.app'; 
-export const APP_URL = 'https://city-mounting-conditioning-role.trycloudflare.com'; 
+export const APP_URL = 'https://orderfix-production-22d8.up.railway.app'; 
